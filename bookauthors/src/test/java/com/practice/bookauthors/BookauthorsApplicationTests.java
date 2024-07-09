aca02052cd92b8449768e7017d9c76cfc238e241
Gregg Milligan II <milligangregg@gmail.com>
2024-07-09 16:05:37 -0400

    init spring boot book authors

---
+++ bookauthors/src/test/java/com/practice/bookauthors/BookauthorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.bookauthors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookauthorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
